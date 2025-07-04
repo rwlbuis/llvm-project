21e1d0eaaa7171e2c77316657048d9d3321f5e86
Michael Buch <michaelbuch12@gmail.com>
2025-07-04 09:18:24 +0100

    [lldb][DataFormatter] Format libstdc++ unique_ptr like we do libc++ (#146909)
    The only difference is that with libc++ the summary string contains the
    derefernced pointer value. With libstdc++ we currently display the
    pointer itself, which seems redundant. E.g.,
    ```
    (std::unique_ptr<int>) iup = 0x55555556d2b0 {
      pointer = 0x000055555556d2b0
    }
    (std::unique_ptr<std::basic_string<char> >) sup = 0x55555556d2d0 {
      pointer = "foobar"
    }
    ```
    
    This patch moves the logic into a common helper that's shared between
    the libc++ and libstdc++ formatters.
    
    After this patch we can combine the libc++ and libstdc++ API tests (see
    https://github.com/llvm/llvm-project/pull/146740).
    
    (cherry picked from commit a89e232058a29260eb9bfe77b862715ce875f962)

--- lldb/test/API/functionalities/data-formatter/data-formatter-stl/libcxx/shared_ptr/main.cpp
+++ lldb/test/API/functionalities/data-formatter/data-formatter-stl/libcxx/shared_ptr/main.cpp
@@ -14,5 +14,9 @@ int main() {
   std::shared_ptr<int> &&sp_int_ref_ref = std::make_shared<int>(10);
   std::shared_ptr<User> sp_user = std::make_shared<User>();
 
+  // Construct empty shared_ptr with non-null control field.
+  std::shared_ptr<int> si(new int(47));
+  std::shared_ptr<int> sie(si, nullptr);
+
   return 0; // break here
 }
